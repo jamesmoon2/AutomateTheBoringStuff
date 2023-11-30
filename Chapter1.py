894c26eacd6288a4ef9408ae37734d71aaf7fccd
james <jamesmoon2@gmail.com>
2023-11-29 21:05:33 -0500

    Settings

--- Chapter1.py
+++ Chapter1.py
@@ -1,1 +1,2 @@
 # Variable Rules - 1. It can only be one word; 2. It can only use letters, numbers, and underscore; 3. It cannot begin with a number.
+# Official python code style dictates using underscores, rather than camel case, for multi-word variables.
---
+++ Chapter2.py
@@ -0,0 +1,8 @@
+# not operator evaluates only one Boolean value or expression
+# you can mix Boolean and comparison operators; Python evaluates not operators first and then and operators and then or operators
+print((4 < 5) and (5 < 6))
+
+# Flow Control
+# a condition is a more specific name in the context of flow control statements; always evaluated down to a Boolean value
+# blocks of code are indicated by indents
+# the most common flow control statement is an if statement; flow control statements end with a colon and are followed by a new block of code (the clause)
