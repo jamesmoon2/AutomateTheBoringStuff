e807c18a745801d50a893cad15aac7693d0d2ed0
james <jamesmoon2@gmail.com>
2023-11-30 21:18:31 -0500

    Finished Chap 2

---
+++ Importing Module.py
@@ -0,0 +1,3 @@
+import random, sys, os, math  #can import multiple modules by delimiting with commas; You can also import by using a statement: from module import *
+for i in range(5):
+    print(random.randint(1, 10))
--- ranges.py
+++ ranges.py
@@ -1,2 +1,3 @@
 for i in range(12, 16, 2): #first number is the start, second number is the stop, third number is the step argument
+    # a negative step number can be used to count down
     print(i)
