fe33caee767a9a1b7c5eb6ca472811808932304c
james <jamesmoon2@gmail.com>
2023-12-12 22:01:26 -0500

    Collatz Sequence code written

--- commaCode.py
+++ commaCode.py
@@ -1,10 +1,19 @@
+# This code should print out apples, bananas, tofu, and cats.
+
 spam = ["apples", "bananas", "tofu", "cats"]
 
 def lister(passedList):
-    listLength = len(passedList)
-    for items in (passedList):
-        print(str(items) + ", ")
-    print(listLength)
+
+    listLength = len(passedList) #calculates length of the list
+    lessOne = listLength - 1 #subtracts one so we know where to add the "and"
+
+
+    for i in (passedList):
+         print(str(i) + ", ",  end="")
+
+
+    #print(listLength)
+    #print(lessOne)
 
 
 
